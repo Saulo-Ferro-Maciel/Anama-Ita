36bebeaea9bf9d989822af41c80004bab68c2728
Saulo-Ferro-Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2022-04-04 12:25:03 -0300

    Add files via upload

---
+++ reminder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear;
+echo "
+>>This little software was made by Saulo Ferro Maciel.
+
+>>Its goal is to help your parents in organizing customer data
+
+Developer Contact:
+ maciel.ferro@acad.ifma.edu.br 
+ kogasaal@gmail.com";
+exit
+
