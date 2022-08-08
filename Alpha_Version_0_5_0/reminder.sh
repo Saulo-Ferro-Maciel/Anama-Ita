37ffb2af9bc72b543d909aae675aebb163698efa
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2022-08-08 14:47:49 -0300

    Last Update

---
+++ Alpha_Version_0_5_0/reminder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear;
+echo "
+>>This little software was made by Saulo Ferro Maciel.
+
+>>Its goal is to help your parents in organizing customer data
+
+>>This version is still being designed, and does not have a binary executable.
+
+Developer Contact:
+ maciel.ferro@acad.ifma.edu.br 
+ kogasaal@gmail.com";
+exit
+
